1def3b828733d191191d565665b796d6f4a1bf54
arctic <icyfreeze5@gmail.com>
2014-01-14 15:27:18 -0800

    c projects

--- cprojects/midpointcalc.c
+++ cprojects/midpointcalc.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 
 int main()
 {
     while(1)
     {
         float x1, x2, y1, y2,x ,y;
-        printf("Midpoint Calculator\n");
-        printf("x1= ");
+        printf("Arctic's Midpoint Calculator\n");
+        printf("Calculates the midpoint of a line formed by the given coordinates\n");
+        printf("Please enter x1: ");
         scanf("%f", &x1);
-        printf("x2= ");
+        printf("Please enter x2: ");
         scanf("%f", &x2);
-        printf("y1= ");
+        printf("Please enter y1: ");
         scanf("%f", &y1);
-        printf("y2= ");
+        printf("Please enter y2: ");
         scanf("%f", &y2);
         //midpoint formula
         x = (x1+x2)/2;
