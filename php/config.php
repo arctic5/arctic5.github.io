fc05374e167516a7b2a2988f4b55370f26daedfb
arctic <icyfreeze5@gmail.com>
2014-08-04 22:09:00 -0700

    restart this from scratch

--- php/config.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-     $db_con = mysql_connect("mysql-w","w2023843admin","jasonwong99");
-          if (!$db_con){
-          die('Could not connect: ' . mysql_error());
-     }
-     mysql_select_db("w2023843_login");
-?>
