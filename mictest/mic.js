7c2e0ab2c740f85273c3eb0b5a94187590c702cc
arctic <icyfreeze5@gmail.com>
2015-06-09 23:57:36 -0700

    afsdfge4r

--- mictest/mic.js
+++ mictest/mic.js
@@ -1,22 +1,11 @@
-navigator.getUserMedia = (navigator.getUserMedia||navigator.webkitGetUserMedia||navigator.mozGetUserMedia||navigator.msGetUserMedia);
-
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var pre = document.querySelector('pre');
-var myScript = document.querySelector('script');
-if (navigator.getUserMedia) {
-    console.log('getUserMedia supported.');
-    navigator.getUserMedia (
-        {audio: true},
-        function(stream) {
-            var source = audioCtx.createMediaStreamSource(stream);
-            var gain = audioCtx.createGain();
-            source.connect(gain);
-            gain.connect(audioCtx.destination);
-        },
-        function(e) {
-            console.log(e);
-        }
-    );
-} else {
-    console.log('u browser suk');
+window.onload = function() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    var context = new AudioContext();
+    navigator.getUserMedia({audio: true}, function(stream) {
+        var microphone = context.createMediaStreamSource(stream);
+        var filter = context.createBiquadFilter();
+        microphone.connect(filter);
+        filter.connect(context.destination);
+    }, function() {alert("ur browser suk");});
 }
