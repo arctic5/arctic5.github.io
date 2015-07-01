8f0a7c7dde44ab59470156ca5d5bbd68d458e906
arctic5 <icyfreeze5@gmail.com>
2015-07-01 00:50:51 -0700

    steal orangestar quote gen

---
+++ js/quotegen.js
@@ -0,0 +1,8 @@
+var quotes = [
+    "zzz school...",
+    "still bad at osu",
+    "still bad at CS:GO",
+
+];
+
+document.getElementById("quote").innerHTML = quotes[Math.floor(Math.random()*(quotes.length))];
