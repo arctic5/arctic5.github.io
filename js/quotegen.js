5e37f653265e634de8aa4f4af07740ed9425accc
jason <arctic5@users.noreply.github.com>
2015-07-23 20:38:14 -0700

    weeb af

--- js/quotegen.js
+++ js/quotegen.js
@@ -2,7 +2,7 @@ var quotes = [
     "zzz school...",
     "still bad at osu",
     "still bad at CS:GO",
-
+    "^ is a huge weeb"
 ];
 
 document.getElementById("quote").innerHTML = quotes[Math.floor(Math.random()*(quotes.length))];
